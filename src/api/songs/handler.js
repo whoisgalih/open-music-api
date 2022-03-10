00414c9989f006c567cfca9db4981ead0d9e73dc
Galih Akbar <galihakbar.ga91@gmail.com>
2022-03-10 21:46:18 +0700

    initial commit

---
+++ src/api/songs/handler.js
@@ -0,0 +1,164 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postSongHandler = this.postSongHandler.bind(this);
+    this.getSongsHandler = this.getSongsHandler.bind(this); // Masih belum ada dalam test
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+  }
+
+  postSongHandler(request, h) {
+    try {
+      this._validator.validateSongPayload(request.payload);
+      // eslint-disable-next-line object-curly-newline
+      const { title, year, performer, genre, duration, albumId } = request.payload;
+
+      const songId = this._service.addSong({
+        title,
+        year,
+        performer,
+        genre,
+        duration,
+        albumId,
+      });
+
+      const response = h.response({
+        status: 'success',
+        message: 'Song berhasil ditambahkan',
+        data: {
+          songId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  getSongsHandler() {
+    const songs = this._service.getSongs();
+    return {
+      status: 'success',
+      data: {
+        songs,
+      },
+    };
+  }
+
+  getSongByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const song = this._service.getSongById(id);
+      return {
+        status: 'success',
+        data: {
+          song,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  putSongByIdHandler(request, h) {
+    try {
+      this._validator.validateSongPayload(request.payload);
+      const { id } = request.params;
+
+      this._service.editSongById(id, request.payload);
+
+      return {
+        status: 'success',
+        message: 'Song berhasil diperbarui',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  deleteSongByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      this._service.deleteSongById(id);
+
+      return {
+        status: 'success',
+        message: 'Song berhasil dihapus',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = SongsHandler;
---
+++ src/services/inMemory/SongsService.js
@@ -0,0 +1,84 @@
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class SongsService {
+  constructor() {
+    this._songs = [];
+  }
+
+  // eslint-disable-next-line object-curly-newline
+  addSong({ title, year, performer, genre, duration }) {
+    const id = `song-${nanoid(16)}`;
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newSong = {
+      title,
+      year,
+      performer,
+      genre,
+      duration,
+      id,
+      createdAt,
+      updatedAt,
+    };
+
+    this._songs.push(newSong);
+
+    const isSuccess = this._songs.filter((song) => song.id === id).length > 0;
+
+    if (!isSuccess) {
+      throw new InvariantError('Song gagal ditambahkan');
+    }
+
+    return id;
+  }
+
+  getSongs() {
+    return this._songs.map((song) => ({
+      id: song.id,
+      title: song.title,
+      performer: song.performer,
+    }));
+  }
+
+  getSongById(id) {
+    const song = this._songs.filter((n) => n.id === id)[0];
+    if (!song) {
+      throw new NotFoundError('Song tidak ditemukan');
+    }
+    return song;
+  }
+
+  // eslint-disable-next-line object-curly-newline
+  editSongById(id, { title, year, performer, genre, duration }) {
+    const index = this._songs.findIndex((song) => song.id === id);
+
+    if (index === -1) {
+      throw new NotFoundError('Gagal memperbarui song. Id tidak ditemukan');
+    }
+
+    const updatedAt = new Date().toISOString();
+
+    this._songs[index] = {
+      ...this._songs[index],
+      title,
+      year,
+      performer,
+      genre,
+      duration,
+      updatedAt,
+    };
+  }
+
+  deleteSongById(id) {
+    const index = this._songs.findIndex((song) => song.id === id);
+    if (index === -1) {
+      throw new NotFoundError('Song gagal dihapus. Id tidak ditemukan');
+    }
+    this._songs.splice(index, 1);
+  }
+}
+
+module.exports = SongsService;
