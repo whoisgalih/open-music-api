f8e91e3cb407967d678cbdb994c2bb69c8201fe6
Galih Akbar <galihakbar.ga91@gmail.com>
2022-03-28 13:15:19 +0700

    open image from link implemented (fixing uploads)

--- src/api/uploads/routes.js
+++ src/api/uploads/routes.js
@@ -1,4 +1,4 @@
-// const path = require('path');
+const path = require('path');
 
 const routes = (handler) => [
   {
@@ -14,15 +14,15 @@ const routes = (handler) => [
       },
     },
   },
-  // {
-  //   method: 'GET',
-  //   path: '/upload/{param*}',
-  //   handler: {
-  //     directory: {
-  //       path: path.resolve(__dirname, 'file'),
-  //     },
-  //   },
-  // },
+  {
+    method: 'GET',
+    path: '/upload/{param*}',
+    handler: {
+      directory: {
+        path: path.resolve(__dirname, 'file'),
+      },
+    },
+  },
 ];
 
 module.exports = routes;
